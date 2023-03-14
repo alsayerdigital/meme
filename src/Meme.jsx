6080c0f2c176b9f958c69dd442964d0c3b7e1fdb
alsayerdigital <robinj@alsayergroup.com>
2023-03-14 11:24:20 +0300

    Added local Payload

--- src/Meme.jsx
+++ src/Meme.jsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import reactLogo from './assets/react.svg'
 import './Meme.css'
+import memeData from './memeData'
 
 export default function Meme() {
+  const [loadImg,setLoadImg] = useState("")
+  function showImage(e){
+    e.preventDefault();
+    const memeArr = memeData.data.memes;
+    const randomMeme = Math.floor(Math.random() * memeArr.length)
+    console.log(memeArr[randomMeme].url)
+    setLoadImg(memeArr[randomMeme].url)
+  }
   return (
     <main>
-        <form className='form'>
+        <form className='form' onSubmit={showImage}>
             <input type="text" placeholder ="Top Text" className="form--input"/>
             <input type="text" placeholder ="Bottom Text" className="form--input"/>
-            <button type="submit" className='form--button'>Submit</button>
+            <button type="submit" className='form--button' >Submit</button>
+            <img src={loadImg}></img>
         </form>
     </main>
   )
